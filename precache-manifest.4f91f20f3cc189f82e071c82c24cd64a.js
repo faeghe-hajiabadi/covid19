add97b35d27609573c9f5a9a45d108a44754b051
Faeghe <faeghe.hajiabadi@gmail.com>
2020-04-01 17:08:46 +0430

    Updates

---
+++ precache-manifest.4f91f20f3cc189f82e071c82c24cd64a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4889ccb7871708c7f97229a65ba9b5",
+    "url": "/faeghe-hajiabadi/covid19/index.html"
+  },
+  {
+    "revision": "4e628b3c28bd5cb0a3ef",
+    "url": "/faeghe-hajiabadi/covid19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c49e671be92b4c9bf721",
+    "url": "/faeghe-hajiabadi/covid19/static/js/2.32be5dab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faeghe-hajiabadi/covid19/static/js/2.32be5dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e628b3c28bd5cb0a3ef",
+    "url": "/faeghe-hajiabadi/covid19/static/js/main.7ada26b2.chunk.js"
+  },
+  {
+    "revision": "becec9513aa3a7397d6f",
+    "url": "/faeghe-hajiabadi/covid19/static/js/runtime-main.ec70f076.js"
+  }
+]);
